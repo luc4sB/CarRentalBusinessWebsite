f051af9e1f7d6e986d53332e14e79f9e98af842e
rowbadow <120333793+rowbadow@users.noreply.github.com>
2024-01-31 14:21:55 +0000

    Added Navbar and react routes

---
+++ src/NavBar.jsx
@@ -0,0 +1,36 @@
+import "./NavBar.css"
+import { Outlet, Link } from "react-router-dom";
+
+
+const navlinks = [
+  {
+    name: "Home",
+    link: "./homepage",
+  },
+  {
+    name: "Login",
+    link: "./login",
+  },
+  
+];
+
+const NavBar = () => {
+
+  return (
+    <>
+      <div className="navbar">
+        <ul>
+          {navlinks.map((item) => (
+              <li key={item.name}>
+  
+                <Link to={item.link}>{item.name}</Link>
+              </li>
+            ))}
+        </ul>
+      </div>
+      <Outlet />
+    </>
+  );
+};
+
+export default NavBar;
