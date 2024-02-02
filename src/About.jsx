1daed1ee50bc179b13965c918a874c9f0fb5dd35
luc4sB <104570692+luc4sB@users.noreply.github.com>
2024-02-02 13:12:44 +0000

    General Improvements
    Added toggle password visibility icon, started to populate about section

--- src/About.jsx
+++ src/About.jsx
@@ -1,11 +1,32 @@
-import "./styles/About.css"
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faTimes, faExchangeAlt, faDollarSign } from '@fortawesome/free-solid-svg-icons';
+import "./styles/About.css";
 
 const About = () => {
-
   return (
     <section id="about-section">
+      <div className="about-item">
+        <FontAwesomeIcon icon={faCheck} className="icon" />
+        <p>Flexible rentals</p>
+      </div>
+
+      <div className="about-item">
+        <FontAwesomeIcon icon={faExchangeAlt} className="icon" />
+        <p>Cancel or change most bookings for free up to 48 hours before pick-up</p>
+      </div>
+
+      <div className="about-item">
+        <FontAwesomeIcon icon={faTimes} className="icon" />
+        <p>No hidden fees</p>
+      </div>
+
+      <div className="about-item">
+        <FontAwesomeIcon icon={faDollarSign} className="icon" />
+        <p>Know exactly what you’re paying, Price Match Guarantee</p>
+      </div>
     </section>
-  )
+  );
 }
 
-export default About
+export default About;
--- src/login.jsx
+++ src/login.jsx
@@ -1,36 +1,52 @@
 import "./styles/Login.css";
 import GoogleLogin from "react-google-login";
 import { useState } from "react";
+import '@fortawesome/fontawesome-free/css/all.css';
 
-function setLoginState() {
-  
-}
+
+function setLoginState() {}
 
 function Login() {
   const responseGoogle = (response) => {
     console.log(response);
-  }
+  };
   const [login, setLogin] = useState("LoggedOut");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   return (
     <>
       <section id="LoginPage">
         <div id="LoginPage-container">
-          
           <h1>Login</h1>
-          
+
           <div id="Login-Inputs">
             <p>Username: </p>
-            <input type="text" id="fname" name="fname"/>
+            <input type="text" id="fname" name="fname" />
             <p>Password: </p>
-            <input type="password" id="fname" name="fname"/>
+            <div className="password-input-container">
+              <input
+                type={showPassword ? "text" : "password"}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                id="pass"
+                name="pass"
+              />
+              <i
+                className={`password-icon ${
+                  showPassword ? "fa fa-eye" : "fa fa-eye-slash"
+                }`}
+                onClick={() => setShowPassword((prev) => !prev)}
+              ></i>
+            </div>
+            <br></br>
             <button id="login-button" onClick={() => setLogin("LoggedIn")}>
               Login
             </button>
             <a>Forgot Password</a>
           </div>
           <hr className="divider"></hr>
-          
+
           <div id="OtherLoginMethods">
             <GoogleLogin
               clientId="984330246636-2cp3h0fti4q2u2n2kq73r0jue5v5b5qf.apps.googleusercontent.com"
@@ -40,13 +56,10 @@ function Login() {
               cookiePolicy={"single_host_origin"}
             />
           </div>
-          
         </div>
-        
       </section>
     </>
   );
 }
-
 
 export default Login;
